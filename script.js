7890d50fbe0e1367ac2dc24127a6295c65a39f68
Michael Lei <michaelleie0ca@gmail.com>
2022-12-29 00:09:02 -0800

    Place marker and swap player turn

--- script.js
+++ script.js
@@ -1,19 +1,47 @@
 const cells = document.querySelectorAll("[data-cell]");
 const board = document.querySelector(".board");
-const switchTurn = (currentPlayer) => {
-	board.classList.remove(currentPlayer);
-	currentPlayer = currentPlayer === "o" ? "x" : "o";
-	return currentPlayer;
-};
-const turn = (currentPlayer) => {
-	board.classList.add(currentPlayer);
-	const placeMove = (e) => {
-		const cell = e.target;
-		cell.classList = `cell ${currentPlayer}`;
-		turn(switchTurn(currentPlayer));
-	};
+const winConditions = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6],
+];
+const x = "x";
+const o = "o";
+
+let circleTurn;
+
+const startGame = () => {
+	circleTurn = true;
 	cells.forEach((cell) => {
-		cell.addEventListener("click", placeMove, { once: true });
+		cell.addEventListener("click", handleClick, { once: true });
 	});
+	setBoardHover();
 };
-turn("o");
+
+const handleClick = (e) => {
+	const cell = e.target;
+	const currentPlayer = circleTurn ? o : x;
+	placeMove(cell, currentPlayer);
+	//Check for Win
+	//Check for Draw
+	switchTurn();
+	setBoardHover();
+};
+
+const placeMove = (cell, currentPlayer) => {
+	cell.classList.add(currentPlayer);
+};
+const switchTurn = () => {
+	circleTurn = !circleTurn;
+};
+const setBoardHover = () => {
+	board.classList.remove(x);
+	board.classList.remove(o);
+	board.classList.add(circleTurn ? o : x);
+};
+startGame();
