61c1f841eb2c783606d7635e17fceda7a1791c8e
Michael Lei <michaelleie0ca@gmail.com>
2022-12-29 00:48:55 -0800

    Add game win logic, function to restart game.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const cells = document.querySelectorAll("[data-cell]");
 const board = document.querySelector(".board");
+const winningMsg = document.getElementById("winningMsg");
+const winText = document.querySelector("[data-winning-msg-text]");
 const winConditions = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -18,6 +20,8 @@ let circleTurn;
 const startGame = () => {
 	circleTurn = true;
 	cells.forEach((cell) => {
+		cell.classList.remove(o);
+		cell.classList.remove(x);
 		cell.addEventListener("click", handleClick, { once: true });
 	});
 	setBoardHover();
@@ -27,10 +31,14 @@ const handleClick = (e) => {
 	const cell = e.target;
 	const currentPlayer = circleTurn ? o : x;
 	placeMove(cell, currentPlayer);
-	//Check for Win
-	//Check for Draw
-	switchTurn();
-	setBoardHover();
+	if (checkWin(currentPlayer)) {
+		endGame(false);
+	} else if (isDraw()) {
+		endGame(true);
+	} else {
+		switchTurn();
+		setBoardHover();
+	}
 };
 
 const placeMove = (cell, currentPlayer) => {
@@ -40,8 +48,32 @@ const switchTurn = () => {
 	circleTurn = !circleTurn;
 };
 const setBoardHover = () => {
-	board.classList.remove(x);
 	board.classList.remove(o);
+	board.classList.remove(x);
 	board.classList.add(circleTurn ? o : x);
 };
+
+const checkWin = (currentPlayer) =>
+	winConditions.some((condition) =>
+		condition.every((i) => cells[i].classList.contains(currentPlayer))
+	);
+const isDraw = () => {
+	return [...cells].every((cell) => {
+		return cell.classList.contains(o) || cell.classList.contains(x);
+	});
+};
+const endGame = (draw) => {
+	winningMsg.classList.add("show");
+	if (draw) {
+		winText.innerText = `Draw!`;
+	} else {
+		const winner = circleTurn ? "O" : "X";
+		winText.innerText = `${winner} wins!`;
+	}
+};
+const restartGame = () => {
+	winningMsg.classList.remove("show");
+	startGame();
+};
+
 startGame();
